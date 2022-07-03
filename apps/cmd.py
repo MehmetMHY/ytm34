918bbcca524cefe145607ff04161652f46c3efd0
mehmet-mhy <mehmet.mhy@gmail.com>
2022-07-04 00:28:26 +0300

    started making main script for command

---
+++ apps/cmd.py
@@ -0,0 +1,21 @@
+import sys
+import os
+
+# create path and load local scripts/module
+PATH = str(os.path.dirname(os.path.realpath(__file__)))
+PATH = PATH.split("/")
+PATH = PATH[0:-1]
+PATH.append("src")
+PATH = "/".join(PATH)
+if not os.path.isdir(PATH):
+    sys.exit()
+sys.path.insert(1, str(PATH))
+
+import console
+import files
+import os
+import yt
+
+
+
+
