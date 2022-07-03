e2ab9b7dd45e84c8105ed7d4fbe42fed550457f6
mehmet-mhy <mehmet.mhy@gmail.com>
2022-07-04 00:29:49 +0300

    minor changes

--- apps/cmd.py
+++ apps/cmd.py
@@ -1,7 +1,8 @@
+# load python/pip-installed modules
 import sys
 import os
 
-# create path and load local scripts/module
+# create path and load local scripts/modules
 PATH = str(os.path.dirname(os.path.realpath(__file__)))
 PATH = PATH.split("/")
 PATH = PATH[0:-1]
@@ -10,12 +11,8 @@ PATH = "/".join(PATH)
 if not os.path.isdir(PATH):
     sys.exit()
 sys.path.insert(1, str(PATH))
-
 import console
 import files
 import os
 import yt
-
-
-
 
